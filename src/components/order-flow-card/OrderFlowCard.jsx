a90745c66d9468017054fbe8fbbc682bce9cc33c
elaineapp082020 <joyfulelaine7@gmail.com>
2021-05-18 22:12:01 -0700

    add Scss Mixins setting

--- src/components/order-flow-card/OrderFlowCard.jsx
+++ src/components/order-flow-card/OrderFlowCard.jsx
@@ -5,8 +5,10 @@ import SvgIcon from '../svg-icon/SvgIcon';
 import './OrderFlowCard.scss';
 
 const OrderFlowCard = ({ id, icon, title, desc }) => {
+  const step = parseInt(id) + 1
   return (
     <div className="card">
+      <span>{0 + step.toString()}</span>
       <div className="card__icon">
         <SvgIcon src={icon} alt={title} />
       </div>
