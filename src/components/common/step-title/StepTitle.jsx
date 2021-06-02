9051d2577957c0646841dbb62972c04921b413d0
elaineapp082020 <joyfulelaine7@gmail.com>
2021-06-01 22:06:40 -0700

    update visual details of order flow page

--- src/components/common/step-title/StepTitle.jsx
+++ src/components/common/step-title/StepTitle.jsx
@@ -6,8 +6,8 @@ const StepTitle = ( {id, title} ) => {
   const step = parseInt(id) + 1
   return (
     <div className="title">
-      <h3>{title}</h3>
       <span>{0 + step.toString()}</span>
+      <h3>{title}</h3>
     </div>
   );
 }
